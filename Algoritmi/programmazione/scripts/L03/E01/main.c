62bae9ecfb99fc460cc142cb60482ff052b39d28
Alexein <alessiocammarata42@gmail.com>
2025-10-16 12:18:25 +0200

    lab3 es1,

--- Algoritmi/programmazione/scripts/L03/E01/main.c
+++ Algoritmi/programmazione/scripts/L03/E01/main.c
@@ -5,6 +5,7 @@ int majority( int *a, int N);
 
 int main(void){
     int N, *a, i;
+
     printf("Inserisci il numero di elementi dell'array: ");
     scanf("%d", &N);
     a = (int *) malloc(N * sizeof(int));
@@ -13,12 +14,20 @@ int main(void){
         return 1;
     }
 
-    printf("Inserisci gli elementi dell'array: ");
+    printf("Inserisci gli elementi dell'array (Naturali, >=0):\n");
     for(i=0; i<N; i++){
+        printf("%d° valore da inserire: ",i+1);
         scanf("%d", &a[i]);
+        while(a[i] < 0){
+            printf("Reinserire il valore (Naturali, >=0): ");
+            scanf("%d", &a[i]);
+        }
     }
-    // a[i] = -1; //Condizione di terminazione
     
+    printf("\nVettore: [");
+    for(int i = 0; i<N-1; i++) printf("%d, ",a[i]);
+    printf("%d].\n",a[N-1]);
+
     int result = majority(a, N);
     if(result != -1)
         printf("Il numero maggioritario è: %d\n", result);
@@ -29,9 +38,43 @@ int main(void){
     return 0;
 }
 
+/*
+    Cerca candidati tra coppie adiacenti (a[i]==a[i+1]).
+    Per ogni candidato conta le occorrenze nell'intero array e ritorna il valore se occ > N/2. 
+    Se non ci sono coppie ed N è dispari, il codice controlla gli indici pari come caso speciale. 
+    Ritorna -1 se non esiste maggioritario.
+    Pero è iterativa non recursive.
+*/
+// int majority(int *a, int N){
+//     int even = (N%2 != 0) ? 0 : 1;
+//     int found = 0, i = 0, occ;
+
+//     for(int j = 0; j < N -1; j++)
+//         // Cerco una coppia, avevo pensato ad una funzione not_in ma non migliora le prestazioni in caso peggiore
+//         if(a[j] == a[j+1]){ 
+//             occ = 0;
+//             for(int k = 0; k<N; k++) 
+//                 if(a[k] == a[j]) occ++;
+//             if (occ>N/2) return a[j]; // Se le occorrenze sono legittime torno il valore
+//             else if (!even){ 
+//                 for(int k = 0, occ = 0; k<N; k+=2) // Conto il caso speciale Es. 1 2 1 2 1
+//                     if(a[k] == a[j]) occ++;
+//                 if (occ>N/2) return a[j];
+//             }
+//         }
+
+//     return -1;
+// }
+
+/*
+    Divide and conquer, complessità O(NlogN).
+    Mi riduco a singole coppie l ed r partendo da sinistra, dopcodiché prendo i valori e li confronto con 
+    gli altri presenti nel vettore per contare quante occorrenze ci sono, e prendo come candidato quello >.
+    Infine controllo se le occ che ho ottenuto siano maggiori di N/2 torno il valore relativo se si senno -1.
+*/
 int majority(int *a, int N){
     if (N <= 0) return -1;
-    /* base case */
+    //Condizione di terminazione
     if (N == 1) return a[0];
 
     int leftN = N / 2;
@@ -40,7 +83,7 @@ int majority(int *a, int N){
     int left = majority(a, leftN);
     int right = majority(a + leftN, rightN);
 
-    if (left == right) return left; // Se i valori sono uguali tipo 1 1 2 3, torna 1
+    if (left == right) return left; // Se i valori sono uguali
 
     int cL = 0, cR = 0;
     for(int i = 0; i<N; i++){
@@ -48,10 +91,12 @@ int majority(int *a, int N){
         if(a[i] == right) cR++;
     }
 
-    int cand = cL > cR ? left : right;
+    // Eseguo il controllo sulle occorrenze che ho contato prima
+    if(cL>cR){ 
+        return cL>N/2 ? left : -1;
+    }else{
+        return cR>N/2 ? right : -1;
+    }
 
-    int occ = 0;
-    for(int i = 0; i<N; i++) if (a[i] == cand) occ++;
-
-    return occ>N/2 ? cand : -1;
+    
 }
---
+++ Algoritmi/programmazione/scripts/L03/E02/main.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXC 51 //50 + 1
+#define MAXL 34 //
+
+typedef struct Item *link, Item; 
+
+typedef enum {
+    r_insert_key, r_insert_file, r_search_id, r_remove_id, r_remove_date, r_stampa, r_fine
+} comando_e;
+
+typedef struct{
+    int year, month, day;
+} date;
+
+struct Item{
+    char codice[MAXC];
+    char nome[MAXC];
+    char cognome[MAXC];
+    date data_di_nascita;
+    char via[MAXC];
+    char citta[MAXC];
+    int cap;
+    link *prev, *next;
+};
+
+// void leggiFile(FILE *fin, int n, Item vet[n], pullman *vetp_orig[n], pullman *vetp_id[n], pullman *vetp_data[n], pullman *vetp_part[n], pullman *vetp_arr[n]);
+comando_e leggiComando(void);
+
+int main(void){
+    FILE *fin;
+    int n = 16, end = 0, count, res;
+
+    Item *vet = malloc(n*sizeof(Item));
+
+    comando_e act;
+    char str[MAXC], ans;
+
+    while (!end) {
+
+        printf("Inserisci la tua scelta:\n");
+        act = leggiComando();
+        switch (act) {
+            case r_insert_key:
+
+                printf("Inserisci due date nel formato (yyyy/mm/dd) :\n");
+
+                break;
+            case r_insert_file:
+
+                printf("Inserisci il nome della fermata:\n");
+                scanf("%s", str);
+
+                if (count == 0) printf("Nessuna corrispondenza\n\n");
+                break;
+            case r_search_id:
+
+                printf("Inserisci il nome della fermata:\n");
+                scanf("%s", str);
+
+                if (count == 0) printf("Nessuna corrispondenza\n\n");
+                break;
+            case r_remove_id:
+
+
+                if (count == 0) printf("Nessuna corrispondenza\n\n");
+                break;
+            case r_remove_date:
+    
+                printf("Inserisce l'id che si vuole controllare:\n");
+                scanf("%s", str);
+
+                if (count == 0) printf("Nessuna corrispondenza\n\n");
+                else printf("Il ritardo accumulato dalla tratta %s è pari a: %d minuti\n\n", str, count);
+
+                break;
+            case r_stampa:
+                int ch;
+
+                // while ((ch = getchar()) != '\n' && ch != EOF);
+                // printf("Inserire il path assoluto del file dove si vuole stampare o 'video' per stampare a video:\n");
+                // fgets(str, buffer, stdin);
+                // str[strcspn(str, "\n")] = 0;
+
+                // if (strcmp(str, "video") == 0) {
+                //     printf("%d\n", n);
+                //     for (int i = 0; i < n; i++) printf("%s %s %s %s %s %s %d\n", vetp[i]->id, vetp[i]->part, vetp[i]->dest, vetp[i]->data, vetp[i]->ora_p, vetp[i]->ora_a, vetp[i]->ret);
+                // } else if (stampaDati(str, n, vetp) == 1) end = 1;
+
+                printf("\nStampa effettuata!\n\n");
+                break;
+            case r_fine:
+                end = 1;
+                break;
+            default:
+                printf("Errore, comando non valido.\n");
+        }
+    }
+
+    return 0;
+}
+
+// void leggiFile(FILE *fin, int n, pullman vet[n], pullman *vetp[n], pullman *vetp_id[n], pullman *vetp_data[n], pullman *vetp_part[n], pullman *vetp_arr[n]){
+//     int i = 0;
+//     while(fscanf(fin, "%s %s %s %s %s %s %d", vet[i].id, vet[i].part, vet[i].dest, vet[i].data, vet[i].ora_p, vet[i].ora_a, &vet[i].ret) == 7 && i < n){
+//         vetp[i] = &vet[i];
+//         vetp_data[i] = &vet[i];
+//         vetp_id[i] = &vet[i];
+//         vetp_part[i] = &vet[i];
+//         vetp_arr[i] = &vet[i];
+//         i++;
+//     }
+
+//     fclose(fin);
+// }
+
+comando_e leggiComando (void) {
+    comando_e c;
+    char cmd[MAXL];
+    char tabella[r_fine + 1][MAXL] = {
+        "inserimento_tastiera","inserimento_file","ricerca_id","cancella_id","ricerca_date","stampa","fine"
+    };
+
+    printf("comando (inserimento_tastiera");
+    printf("/inserimento_file/ricerca_id/cancella_id/ricerca_date/stampa/fine): ");
+    scanf("%s",cmd); 
+    c=r_insert_key;
+
+    while(c<r_fine+1 && strcmp(cmd,tabella[c])!=0)
+        c++;
+
+    return (c);
+}
