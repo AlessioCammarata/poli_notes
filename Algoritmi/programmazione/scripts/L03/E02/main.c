0d43d9b89d398a9f8b0e2d14dfe8233df6649b75
Alexein <alessiocammarata42@gmail.com>
2025-10-16 23:46:56 +0200

    lab3 es2 inizio

--- Algoritmi/programmazione/scripts/L03/E02/main.c
+++ Algoritmi/programmazione/scripts/L03/E02/main.c
@@ -4,6 +4,7 @@
 
 #define MAXC 51 //50 + 1
 #define MAXL 34 //
+#define STANDARD_SIZE 16
 
 typedef struct Item *link, Item; 
 
@@ -19,72 +20,53 @@ struct Item{
     char codice[MAXC];
     char nome[MAXC];
     char cognome[MAXC];
-    date data_di_nascita;
+    date data;
     char via[MAXC];
     char citta[MAXC];
     int cap;
-    link *prev, *next;
+    link prev, next;
 };
 
-// void leggiFile(FILE *fin, int n, Item vet[n], pullman *vetp_orig[n], pullman *vetp_id[n], pullman *vetp_data[n], pullman *vetp_part[n], pullman *vetp_arr[n]);
+FILE *nuovo_file();
+int controlla_anzianita(date *a, date *b);
+void inserisci_ord(link *head, link p);
+int inserisci_tastiera(link *head);
+void leggiFile(FILE *fin, int *n, Item *vet);
 comando_e leggiComando(void);
 
 int main(void){
     FILE *fin;
-    int n = 16, end = 0, count, res;
+    int size = STANDARD_SIZE, n = 0, end = 0, count = 0, res;
 
-    Item *vet = malloc(n*sizeof(Item));
+    // Item *vet = malloc(size*sizeof(Item));
+    link head = NULL;
 
     comando_e act;
-    char str[MAXC], ans;
+    char str[MAXC], ans, ch;
 
     while (!end) {
 
         printf("Inserisci la tua scelta:\n");
         act = leggiComando();
+        // Consuma e scarta tutti i caratteri rimanenti nel buffer di input fino al carattere di nuova linea ('\n') o alla fine del file (EOF).
+        while ((ch = getchar()) != '\n' && ch != EOF);
+
         switch (act) {
             case r_insert_key:
 
-                printf("Inserisci i dati da tastiera nell'ordine: () :\n");
+                while(inserisci_tastiera(&head)) n++;
+
+                printf("\n%d elementi nel vettore\n",n);
 
                 break;
             case r_insert_file:
                 if ((fin = nuovo_file()) != NULL) {
+ 
+                    leggiFile(fin, &n, head); // Si suppone che gli elementi non siano gia presenti
                         
-                    if (fscanf(fin, "%d", &n) == 1 && n <= MAXR) {
-                        // Prima di leggere un nuovo file:
-                        // free(vet);
-                        // free(vetp_orig);
-                        // free(vetp_id);
-                        // free(vetp_data);
-                        // free(vetp_part);
-                        // free(vetp_arr);
-
-                        // // Dopo aver letto n dal nuovo file:
-                        // vet = malloc(n * sizeof(pullman));
-                        // vetp_orig = malloc(n * sizeof(pullman *));
-                        // vetp_id   = malloc(n * sizeof(pullman *));
-                        // vetp_data = malloc(n * sizeof(pullman *));
-                        // vetp_part = malloc(n * sizeof(pullman *));
-                        // vetp_arr  = malloc(n * sizeof(pullman *));
-
-                        if(vet && vetp_orig && vetp_id && vetp_data && vetp_part && vetp_arr){
-                            leggiFile(fin, n, vet, vetp_orig, vetp_id, vetp_data, vetp_part, vetp_arr);
-                            vetp = vetp_orig;
-                            
-                        }else{
-                            printf("Errore di malloc per il path\n");                                                            fclose(fin);
-                            fclose(fin);
-                            end = 1;
-                        }
-                    }else{
-                        printf("Errore, ci sono piu campi di quanti ce ne dvorebbero\n.");
-                        fclose(fin);
-                        end = 1;
-                    }
                 }else printf("Errore in apertura del file\n");
 
-                break;
+                break;  
             case r_search_id:
 
                 printf("Inserisci il nome della fermata:\n");
@@ -107,8 +89,11 @@ int main(void){
 
                 break;
             case r_stampa:
-                int ch;
-
+                // int ch;
+                link p = malloc(sizeof *p);
+                for(p=head; p!=NULL; p = p->next){
+                    printf("%s\n",p->codice);
+                }
                 // while ((ch = getchar()) != '\n' && ch != EOF);
                 // printf("Inserire il path assoluto del file dove si vuole stampare o 'video' per stampare a video:\n");
                 // fgets(str, buffer, stdin);
@@ -129,22 +114,82 @@ int main(void){
         }
     }
 
+    // free_list(&head); // libera tutti i nodi prima di uscire
     return 0;
 }
 
-// void leggiFile(FILE *fin, int n, pullman vet[n], pullman *vetp[n], pullman *vetp_id[n], pullman *vetp_data[n], pullman *vetp_part[n], pullman *vetp_arr[n]){
-//     int i = 0;
-//     while(fscanf(fin, "%s %s %s %s %s %s %d", vet[i].id, vet[i].part, vet[i].dest, vet[i].data, vet[i].ora_p, vet[i].ora_a, &vet[i].ret) == 7 && i < n){
-//         vetp[i] = &vet[i];
-//         vetp_data[i] = &vet[i];
-//         vetp_id[i] = &vet[i];
-//         vetp_part[i] = &vet[i];
-//         vetp_arr[i] = &vet[i];
-//         i++;
-//     }
+FILE *nuovo_file(){
+    int len_str = 0, size_str = STANDARD_SIZE*2;        
+    FILE *fin;
+    char c, *path = (char *)malloc(size_str * sizeof(char));
 
-//     fclose(fin);
-// }
+    if(!path){
+        printf("Errore di malloc per il path\n");
+        return NULL;
+    }
+
+    printf("Inserire il path del file da analizzare\n");
+    while((c = getchar()) != '\n' && c != EOF){
+        if (len_str >= size_str -1){
+            size_str *= 2;
+            path = (char *)realloc(path, size_str*sizeof(char));
+            if(!path){
+                printf("Errore di realloc per il path\n");
+                free(path);
+                return NULL;
+            }
+        }
+        path[len_str++] = (char)c;
+    }
+    path[len_str] = '\0'; 
+
+    fin = fopen(path, "r");
+    free(path);
+    return fin;
+}
+
+int controlla_anzianita(date *a, date *b){
+
+}
+
+void inserisci_ord(link *head, link p){
+    
+}
+
+int inserisci_tastiera(link *head) {
+    link p = malloc(sizeof *p);
+    if (!p) { fprintf(stderr, "malloc fallita\n"); return 0; }
+
+    printf("Inserisci: <codice> <nome> <cognome> gg/mm/aaaa <via> <citta> <cap>\n");
+    int scanned = scanf("%50s %50s %50s %d/%d/%d %50s %50s %d",
+        p->codice, p->nome, p->cognome,
+        &p->data.day, &p->data.month, &p->data.year,
+        p->via, p->citta, &p->cap);
+
+    if (scanned != 9) {
+        free(p);
+        return 0;
+    }
+
+    /* inizializza i puntatori della lista prima dell'inserimento */
+    p->prev = p->next = NULL;
+
+    /* inserisci ordinato (implementa insert_ordered per l'ordinamento per anzianità) */
+    insert_ordered(head, p);
+
+    return 1;
+}
+
+void leggiFile(FILE *fin, int *n, Item *vet){
+    int i = *n;
+
+    while(fscanf(fin, "%50s %50s %50s %d/%d/%d %50s %50s %d",vet[i].codice, vet[i].nome, vet[i].cognome, &vet[i].data.day, &vet[i].data.month, &vet[i].data.year, vet[i].via, vet[i].citta, &vet[i].cap) == 9){
+        i++;
+    }
+    
+    *n = i;
+    fclose(fin);
+}
 
 comando_e leggiComando (void) {
     comando_e c;
@@ -163,3 +208,4 @@ comando_e leggiComando (void) {
 
     return (c);
 }
+
