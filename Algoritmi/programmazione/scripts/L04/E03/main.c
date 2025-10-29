259af62fa8dae667ac4d1bf22e4968d8a722d10a
alexein1001 <alessiocammarata42@gmail.com>
2025-10-29 09:58:26 +0100

    lab4 finito

--- Algoritmi/programmazione/scripts/L04/E03/main.c
+++ Algoritmi/programmazione/scripts/L04/E03/main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 // #define nfin "D:/politecnico/poli_notes/Algoritmi/programmazione/scripts/L04/E03/easy_test_set.txt"
-#define nfin "./easy_test_set.txt"
+#define nfin "./hard_test_set.txt"
 #define Nstones 4
+
+typedef enum{
+    Z,R,T,S
+}stones;
 
 void free_vet(int **vet, int n);
 int leggiFile(FILE *fin, int **all_stones, int n);
@@ -38,20 +42,22 @@ int main(void){
     }
     fclose(fin);
 
-    for(int k = 0; k<n; k++){
-        printf("zaffiro = %d, rubino = %d, topazio = %d, smeraldo = %d, TOT = %d\n",all_stones[k][0],all_stones[k][1],all_stones[k][2],all_stones[k][3],all_stones[k][0]+all_stones[k][1]+all_stones[k][2]+all_stones[k][3]);
-        len = necklace_maker(all_stones[k], -1);
+    for(int k = 0, start; k<n; k++){
+        printf("zaffiro = %d, rubino = %d, topazio = %d, smeraldo = %d, TOT = %d\n",all_stones[k][Z],all_stones[k][R],all_stones[k][T],all_stones[k][S],all_stones[k][Z]+all_stones[k][R]+all_stones[k][T]+all_stones[k][S]);
+        // start: se R/T entrambi zero, la len è il massimo tra Z ed S
+        if (all_stones[k][R] == 0 && all_stones[k][T] == 0) {
+            start = (all_stones[k][Z] > 0) ? Z : S;
+            len = all_stones[k][start];
+        } else {
+            start = (all_stones[k][R] > all_stones[k][T]) ? R : T;
+            len = necklace_maker(all_stones[k], start);
+        }
+
         printf("Collana massima di lunghezza %d\n",len);
+        //Libero la memora appena letta
         free(all_stones[k]);
     }
     
-    // for(int i = 0, j; i<n; i++){
-    //     for(j = 0; j<Nstones;j++) printf("%d ",stones[i][j]);
-    //     printf(" -> %d pietre",stones[i][j]);
-    //     printf("\n");
-    // }
-
-    // free_vet(all_stones, n);
     free(all_stones);
     return 0;
 }
@@ -88,69 +94,34 @@ int leggiFile(FILE *fin, int **all_stones, int n){
     return 1;
 }
 
-// Indici: 0=Z, 1=R, 2=T, 3=S
-int is_ZS(int x) { return x == 0 || x == 3; }
-int is_RT(int x) { return x == 1 || x == 2; }
-
-int can_follow(int last, int nxt) {
-    if (last < 0) return 1;            // primo elemento libero
-    return (is_ZS(last) && is_RT(nxt)) ||
-           (is_RT(last) && is_ZS(nxt));
-}
-
-int best_necklace(int last, int cnt[4]) {
-    int best = 0, moved = 0;
-    for (int nxt = 0; nxt < 4; ++nxt) {
-        if (cnt[nxt] == 0) continue;
-        if (!can_follow(last, nxt)) continue;
-        moved = 1;
-        cnt[nxt]--;
-        int len = 1 + best_necklace(nxt, cnt);
-        if (len > best) best = len;
-        cnt[nxt]++;
-    }
-    return moved ? best : 0; // nessuna mossa: termina
-}
-
-int necklace_maker(int *stones, int pos) {
-    int cnt[4] = { stones[0], stones[1], stones[2], stones[3] };
-
-    if (pos >= 0) {
-        if (cnt[pos] == 0) return 0;
-        cnt[pos]--;
-        return 1 + best_necklace(pos, cnt);
-    }
-
-    int best = 0;
-    for (int start = 0; start < 4; ++start) {
-        if (cnt[start] == 0) continue;
-        cnt[start]--;
-        int len = 1 + best_necklace(start, cnt);
-        if (len > best) best = len;
-        cnt[start]++;
-    }
-    return best;
-}
-
 // Ordine: z, r, t, s
+int next_stones_avaibility(int *stones, int pos){
+    if(pos == Z) pos = stones[Z] == 0 ? R : Z; //Finisco gli zaffiri e passo ai rubini
+    else if(pos == R) pos = stones[T] > 1 && (stones[R] != 0) ? T : S; //Finche ci sono topazi e rubini il prossimo è topazio
+    else if(pos == T) pos = stones[R] > 1 && (stones[T] != 0)? R : Z; //Finche ci sono rubini e topazi il prossimo è rubino
+    else if(pos == S) pos = stones[S] == 0 ? T : S; // Finisco gli smeraldi poi passo ai topzai
+
+    return pos;
+}
+
 // Disposizioni ripetute con criterio
-// int necklace_maker(int *stones, int pos){
-//     int len = 0;
-//     printf("%d\n",pos);
-//     if(stones[pos] != 0){
-//         stones[pos]--;
-//         for(int j = 0; j<Nstones;j++) printf("%d ",stones[j]);
-//         printf("\n");
-//         return 1;
-//     }
+//Parto o dai rubini o dai topazi perche formano un ciclo tra di loro, se non ci sono il caso è banale
+int necklace_maker(int *stones, int pos){
+    int len = 0;
+    //stones[next_stones_avaibility(stones,pos)]
+    if(stones[pos] <= 0){
+        // printf("\n%d %d",stones[pos],pos);
+        printf("\n");
+        for(int j = 0; j<Nstones;j++) printf("%d ",stones[j]);
+        printf("\n");
+        return len;
+    }
+    stones[pos]--;
+    // printf("%d ",pos);
+    pos = next_stones_avaibility(stones,pos);
 
-//     if(pos == 0) pos = stones[pos] == 0 ? 1 : 0; //Finisco gli zaffiri e passo ai rubini
-//     else if(pos == 1) pos = stones[3] == 0 ? 2 : 3; // Finisco gli smeraldi poi passo ai topzai
-//     else if(pos == 2) pos = stones[0] == 0 ? 1 : 0;
-//     else if(pos == 3) pos = stones[pos] == 0 ? 2 : 3; //Finisco gli smeraldi e passo a topazi
+    len = necklace_maker(stones, pos) + 1;
 
-//     len += necklace_maker(stones, pos);
+    return len;
 
-//     return len;
-
-// }
+}
